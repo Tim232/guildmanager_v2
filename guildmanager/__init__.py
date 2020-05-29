767a59b28b12838630a7638cfb491a45b58a60e3
dragdev <samueljohnnicholson@gmail.com>
2020-05-29 03:22:17 +0100

    Add average ping

--- guildmanager/__init__.py
+++ guildmanager/__init__.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from datetime import datetime
 from jishaku.paginators import PaginatorEmbedInterface
 
@@ -25,7 +25,12 @@ class GuildManager(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+        self.pings = 0.0
+        self.average_latency = float(self.bot.latency / 1000)
+
         self.loaded = datetime.utcnow()
+
         try:
             with open("./gman.data") as rfile:
                 data = json.load(rfile)
@@ -37,12 +42,42 @@ class GuildManager(commands.Cog):
             with open("./gman.data", "w+") as wfile:
                 json.dump(_DEFAULTS, wfile)
                 self.data = _DEFAULTS
+        finally:
+            logging.log("[GUILDMANAGER] Cog Loaded.")
+            self.sample_ping.start()
+
+    def cog_unload(self):
+        logging.info(f"[GUILDMANAGER] Cog unloaded.")
+        self.sample_ping.stop()
 
     async def cog_check(self, ctx: commands.Context):
         if not await ctx.bot.is_owner(ctx.author):
             raise commands.NotOwner()
 
         return True
+
+    @property
+    def ping(self) -> float:
+        """Returns the bot's average latency (heartbeat/api connection latency), in ms.
+
+        This is sampled every second."""
+        return self.average_latency
+
+    @property
+    def sampled_pings(self) -> float:
+        """returns how many times the cog has sampled pings."""
+        return self.pings
+
+    @tasks.loop(seconds=1)
+    async def sample_ping(self):
+        """
+        Samples pings every second.
+        **Do NOT overwrite this!**
+        """
+        self.pings += 1
+        latency = self.bot.latency / 1000  # convert to ms
+        self.average_latency += latency
+        self.average_latency /= self.pings
 
     @commands.command(name="guilds", aliases=['servers', 'gm', 'sm'])
     @commands.bot_has_permissions(**_PERMS)
@@ -59,6 +94,8 @@ class GuildManager(commands.Cog):
                   f"**Channels:** {len(list(self.bot.get_all_channels()))}\n"
                   f"**Users:** {len(self.bot.users)}\n"
                   f"**Emojis:** {len(set(self.bot.emojis))}\n"
+                  f"**Cached Messages:** {len(self.bot.cached_messages)}\n"
+                  f"**Average Ping:** `{self.average_latency}ms`\n"
                   f"\n"
                   f"**Loaded Cogs:** {len(self.bot.cogs)}\n"
                   f"**Loaded Extensions:** {len(self.bot.extensions)}\n"
