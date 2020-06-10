0ce129390f3cf55877dafb059381a5d72bd227dc
dragdev <samueljohnnicholson@gmail.com>
2020-06-10 22:38:15 +0100

    update version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open('README.md') as f:
 
 setup(
     name='guildmanager-v2',
-    version="0.0.4",  # version,
+    version="0.0.5",  # version,
     packages=['guildmanager'],
     url='https://github.com/dragdev-studios/guildmanager_v2',
     license='Apache License 2.0',
